313e6d8ff7cc40072a07e1ff94ac1115193e5c47
radiantrivals <ftc.team.6103@gmail.com>
2022-10-11 17:53:15 -0500

    Turned off disabled

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LiftTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LiftTester.java
@@ -51,7 +51,7 @@ import com.qualcomm.robotcore.util.Range;
  */
 
 @TeleOp(name="Lift Tester", group="Iterative Opmode")
-@Disabled
+
 public class LiftTester extends OpMode
 {
     // Declare OpMode members.
