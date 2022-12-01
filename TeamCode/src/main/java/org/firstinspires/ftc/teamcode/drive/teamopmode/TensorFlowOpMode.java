46e550ccdfdb05996fb3ed3e03f70a0d8528e2ca
Dustin Good <dustingood@gmail.com>
2022-12-01 17:33:34 -0600

    12/1 - Autonomous changes for randomization check

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teamopmode/TensorFlowOpMode.java
@@ -0,0 +1,121 @@
+/* Copyright (c) 2019 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.drive.teamopmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
+
+public class TensorFlowOpMode extends LinearOpMode {
+
+    private static final String TFOD_MODEL_ASSET = "PowerPlay.tflite";
+
+    private static final String[] LABELS = {"1 Bolt","2 Bulb","3 Panel"};
+
+    private static final String VUFORIA_KEY =
+            "AbZto7//////AAABmXe8TLnlI0uXt5xM9s/AfsQQU604XQ55/gssDzCv6mwTGtVo8f1zJUtlUoecznZRKTNtsho5fZFvqY8zOh08BSe2ivI3KBBUVWfuqG7PXOUib5X4dZ1n+dsgFdNvgHVqdBR7VqOQqSStBmLE3/LB6NWqJxlLtI5Yb485dXuat+oOf+A2t57wGcY5wmvfwKhRbKjdCEJR0vQcViRfVWQ9kBdRo8XXx2c5itH3TdWIDf3vofiJXu5nWfALDMdBjeXIOczEnT8iYSJOvjZF1XbtUWVo+BfBK08tY2ctjo6MBWf3CEtVZb6yQUtDT6RM8NTh2p7h0SyW/sGkN5Et0H+zYbBQU6RM0xM/ilaJYsPhNzmy";
+
+    private String label = "";
+    private float confidence = 0.0f;
+
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+    }
+
+    protected void initTensorFlow(){
+        initVuforia();
+        initTfod();
+
+        if (tfod != null) {
+            tfod.activate();
+            tfod.setZoom(1.0, 16.0/9.0);
+        }
+
+        telemetry.addData(">", "Press Play to start op mode");
+        telemetry.update();
+    }
+
+    protected String detectSignal(){
+
+        if (tfod != null){
+            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+            if (updatedRecognitions != null) {
+                for (Recognition recognition : updatedRecognitions) {
+                    if (!label.equalsIgnoreCase(recognition.getLabel()) && recognition.getConfidence() > confidence){
+                        confidence = recognition.getConfidence();
+                        label = recognition.getLabel();
+                    }
+                }
+            }
+        }
+
+        telemetry.update();
+
+        return label;
+    }
+
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = CameraDirection.BACK;
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+    }
+
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.75f;
+        tfodParameters.isModelTensorFlow2 = true;
+        tfodParameters.inputSize = 300;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+
+        // Use loadModelFromAsset() if the TF Model is built in as an asset by Android Studio
+        // Use loadModelFromFile() if you have downloaded a custom team model to the Robot Controller's FLASH.
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
+        // tfod.loadModelFromFile(TFOD_MODEL_FILE, LABELS);
+    }
+}
