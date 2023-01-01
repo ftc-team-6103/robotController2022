685ed4a86e6aa35bcd04ac5be4f9661fda23d31c
radiantrivals <ftc.team.6103@gmail.com>
2023-01-01 13:57:55 -0600

    Modifications for new image detection model

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teamopmode/TensorFlowOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teamopmode/TensorFlowOpMode.java
@@ -127,9 +127,9 @@ public class TensorFlowOpMode extends LinearOpMode {
         int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
                 "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfodParameters.minResultConfidence = 0.75f;
+        tfodParameters.minResultConfidence = 0.9f;
         tfodParameters.isModelTensorFlow2 = true;
-        tfodParameters.inputSize = 300;
+        //tfodParameters.inputSize = 300;
 
         tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
 
