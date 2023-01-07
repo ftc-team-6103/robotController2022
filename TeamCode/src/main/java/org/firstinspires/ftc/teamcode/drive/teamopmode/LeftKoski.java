a91357c836453ac032cfb150884e685dde232e2d
Dustin Good <dustingood@gmail.com>
2023-01-07 12:05:21 -0600

    1/7 Added opmode for testing trajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teamopmode/LeftKoski.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teamopmode/LeftKoski.java
@@ -2,11 +2,8 @@ package org.firstinspires.ftc.teamcode.drive.teamopmode;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
-import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryAccelerationConstraint;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -14,10 +11,8 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.hardware.Arm;
 import org.firstinspires.ftc.teamcode.hardware.Claw;
 import org.firstinspires.ftc.teamcode.hardware.Lift;
-import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 @Config
-@Disabled
 @Autonomous(group = "drive")
 public class LeftKoski extends TensorFlowOpMode {
 
@@ -28,24 +23,16 @@ public class LeftKoski extends TensorFlowOpMode {
     private Lift lift;
     private DcMotor liftMotor;
 
-    private Pose2d poseHome = new Pose2d(0,0,Math.toRadians(0));
-
-    private Pose2d poseBackup = new Pose2d(-24.87, 0, 0); //2.47
-    private Pose2d poseRotateToPole = new Pose2d(-28.86, 3.53, 2.47); //2.47
-    private Pose2d poseInitialPole = new Pose2d(-35.28, 7.5, 2.47); //2.47
-
-    private Pose2d poseRotateAroundPole = new Pose2d(-35.28, 7.5, 1.57);
-    private Pose2d poseBackAwayFromPole = new Pose2d(-35.28, 0, 1.57);
-    private Pose2d poseStrafeConeOutOfWay = new Pose2d(-45.28, 0, 1.57);
-
+    private Pose2d poseHome = new Pose2d(0,0,0);
+    private Pose2d poseBackup = new Pose2d(-36.15,0,0);
+    private Pose2d poseMediumPole = new Pose2d(-29.29,2.77, -0.8);
 
     private Pose2d poseParking1 = new Pose2d(-31.29, -26, 0);
     private Pose2d poseParking2 = new Pose2d(-29.29, 0, 0);
     private Pose2d poseParking3 = new Pose2d(-29.29, 26, 0);
 
-    private TrajectorySequence sequencePlaceInitialCone = null;
-    private TrajectorySequence sequenceMoveAwayFromPole = null;
-
+    private Trajectory trajectoryHomeToBackUp = null;
+    private Trajectory trajectoryBackUpToPole = null;
     private Trajectory trajectoryPoleToParkingPosition1 = null;
     private Trajectory trajectoryPoleToParkingPosition2 = null;
     private Trajectory trajectoryPoleToParkingPosition3 = null;
@@ -53,7 +40,7 @@ public class LeftKoski extends TensorFlowOpMode {
     private String randomization = "";
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() throws InterruptedException{
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
@@ -68,73 +55,74 @@ public class LeftKoski extends TensorFlowOpMode {
 
         waitForStart();
 
-//        double start = this.getRuntime();
-//
-//        while (this.getRuntime() < start + 2) {
-//            telemetry.addData("runtime", this.getRuntime());
-//            telemetry.update();
-//            randomization = detectSignal();
-//        }
-//
-//        telemetry.addData("randomization", "detected: " + randomization);
-//        telemetry.update();
-//
-//
-//        claw.close();
-//        sleep(250);
-//        lift.adjustUp();
+        double start = this.getRuntime();
 
-        drive.setPoseEstimate(poseHome);
+        while (this.getRuntime() < start + 2){
+            telemetry.addData("runtime", this.getRuntime());
+            telemetry.update();
+            randomization = detectSignal();
+        }
+
+        telemetry.addData("randomization", "detected: " + randomization);
+        telemetry.update();
+
 
         claw.close();
-        sleep(500);
-        arm.rotateForward();
-        lift.moveToPositionAsync(Lift.POSITION_MID_TERMINAL);
+        sleep(1000);
+        lift.adjustUp();
 
-        sequencePlaceInitialCone = drive.trajectorySequenceBuilder(poseHome)
+        trajectoryHomeToBackUp = drive.trajectoryBuilder(poseHome)
                 .lineToLinearHeading(poseBackup)
-                .lineToLinearHeading(poseRotateToPole)
-                .lineToLinearHeading(poseInitialPole)
                 .build();
 
-        drive.followTrajectorySequence(sequencePlaceInitialCone);
+        drive.followTrajectory(trajectoryHomeToBackUp);
 
+        lift.moveToPosition(Lift.POSITION_MID_TERMINAL);
+
+        trajectoryBackUpToPole = drive.trajectoryBuilder(poseBackup)
+                .lineToLinearHeading(poseMediumPole)
+                .build();
+
+        drive.followTrajectory(trajectoryBackUpToPole);
+
+        sleep(1000);
+        arm.rotateRear();
+        sleep(2000);
+        lift.moveToPosition(Lift.POSITION_MID_TERMINAL + 350);
+        sleep(1000);
         claw.open();
-
-        sequenceMoveAwayFromPole = drive.trajectorySequenceBuilder(poseInitialPole)
-                .lineToLinearHeading(poseRotateAroundPole)
-                .lineToLinearHeading(poseBackAwayFromPole)
-                .lineToLinearHeading(poseStrafeConeOutOfWay)
-                .build();
-
-        drive.followTrajectorySequence(sequenceMoveAwayFromPole);
+        sleep(1000);
+        arm.rotateForward();
+        sleep(1000);
+        driveToParkingPosition(drive);
+        lift.moveToPosition(Lift.POSITION_GROUND);
     }
 
-//    private void driveToParkingPosition(SampleMecanumDrive drive){
-//        trajectoryPoleToParkingPosition1 = drive.trajectoryBuilder(poseMediumPole)
-//                .lineToLinearHeading(poseParking1)
-//                .build();
-//        trajectoryPoleToParkingPosition2 = drive.trajectoryBuilder(poseMediumPole)
-//                .lineToLinearHeading(poseParking2)
-//                .build();
-//        trajectoryPoleToParkingPosition3 = drive.trajectoryBuilder(poseMediumPole)
-//                .lineToLinearHeading(poseParking3)
-//                .build();
-//
-//        int parking = 3;
-//
-//        if (randomization != null && randomization.trim().length() > 0){
-//            parking = Integer.parseInt(randomization.substring(0,1));
-//        }
-//
-//        if (parking == 1){
-//            drive.followTrajectory(trajectoryPoleToParkingPosition1);
-//        }
-//        else if (parking == 2){
-//            drive.followTrajectory(trajectoryPoleToParkingPosition2);
-//        }
-//        else{
-//            drive.followTrajectory(trajectoryPoleToParkingPosition3);
-//        }
-//    }
+    private void driveToParkingPosition(SampleMecanumDrive drive){
+        trajectoryPoleToParkingPosition1 = drive.trajectoryBuilder(poseMediumPole)
+                .lineToLinearHeading(poseParking1)
+                .build();
+        trajectoryPoleToParkingPosition2 = drive.trajectoryBuilder(poseMediumPole)
+                .lineToLinearHeading(poseParking2)
+                .build();
+        trajectoryPoleToParkingPosition3 = drive.trajectoryBuilder(poseMediumPole)
+                .lineToLinearHeading(poseParking3)
+                .build();
+
+        int parking = 3;
+
+        if (randomization != null && randomization.trim().length() > 0){
+            parking = Integer.parseInt(randomization.substring(0,1));
+        }
+
+        if (parking == 1){
+            drive.followTrajectory(trajectoryPoleToParkingPosition1);
+        }
+        else if (parking == 2){
+            drive.followTrajectory(trajectoryPoleToParkingPosition2);
+        }
+        else{
+            drive.followTrajectory(trajectoryPoleToParkingPosition3);
+        }
+    }
 }
