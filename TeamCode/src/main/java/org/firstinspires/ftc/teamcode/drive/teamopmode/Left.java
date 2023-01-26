21cbbe77c23a85358ea9825604ac0558d3f15700
Dustin Good <dustingood@gmail.com>
2023-01-26 07:52:56 -0600

    Added Left POC

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teamopmode/Left.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teamopmode/Left.java
@@ -4,6 +4,7 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -14,6 +15,7 @@ import org.firstinspires.ftc.teamcode.hardware.Claw;
 import org.firstinspires.ftc.teamcode.hardware.Lift;
 
 @Config
+@Disabled
 @Autonomous(group = "drive")
 public class Left extends TensorFlowOpMode {
 
